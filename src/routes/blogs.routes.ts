8544048f1e2e1b9384e08fa69731628ba323aa68
Dave <d06163606@gmail.com>
2025-11-04 20:52:21 +0700

    fix: fix blog route conflict for slug

--- src/routes/blogs.routes.ts
+++ src/routes/blogs.routes.ts
@@ -3,8 +3,8 @@ import { BlogsController } from "../controllers";
 
 export const blogsRoutes = new Hono()
 	.get("/", BlogsController.getAllBlogs)
+	.get("/slug/:slug", BlogsController.getBlogBySlug) // Must be before /:id
 	.get("/:id", BlogsController.getBlogById)
-	.get("/slug/:slug", BlogsController.getBlogBySlug)
 	.post("/", BlogsController.createBlog)
 	.put("/:id", BlogsController.updateBlog)
 	.delete("/:id", BlogsController.deleteBlog);

8c9228ec38298192bdd6825e82be2bb9cf8fad92
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-12-31 10:23:43 +0700

    chore: add migration script to rename columns to snake_case, add new fields to account and session tables, and update foreign key constraints

---
+++ drizzle/0003_awesome_dexter_bennett.sql
@@ -0,0 +1,33 @@
+ALTER TABLE "account" RENAME COLUMN "accountId" TO "account_id";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "providerId" TO "provider_id";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "userId" TO "user_id";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "createdAt" TO "created_at";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "updatedAt" TO "updated_at";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "expiresAt" TO "expires_at";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "createdAt" TO "created_at";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "updatedAt" TO "updated_at";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "userId" TO "user_id";--> statement-breakpoint
+ALTER TABLE "user" RENAME COLUMN "emailVerified" TO "email_verified";--> statement-breakpoint
+ALTER TABLE "user" RENAME COLUMN "createdAt" TO "created_at";--> statement-breakpoint
+ALTER TABLE "user" RENAME COLUMN "updatedAt" TO "updated_at";--> statement-breakpoint
+ALTER TABLE "verification" RENAME COLUMN "expiresAt" TO "expires_at";--> statement-breakpoint
+ALTER TABLE "verification" RENAME COLUMN "createdAt" TO "created_at";--> statement-breakpoint
+ALTER TABLE "account" DROP CONSTRAINT "account_userId_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "session" DROP CONSTRAINT "session_userId_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "access_token" text;--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "refresh_token" text;--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "id_token" text;--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "access_token_expires_at" timestamp;--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "refresh_token_expires_at" timestamp;--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "scope" text;--> statement-breakpoint
+ALTER TABLE "session" ADD COLUMN "ip_address" text;--> statement-breakpoint
+ALTER TABLE "session" ADD COLUMN "user_agent" text;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN "image" text;--> statement-breakpoint
+ALTER TABLE "verification" ADD COLUMN "updated_at" timestamp NOT NULL;--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "session" ADD CONSTRAINT "session_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "account_userId_idx" ON "account" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "session_userId_idx" ON "session" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "verification_identifier_idx" ON "verification" USING btree ("identifier");
